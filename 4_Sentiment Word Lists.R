1f182b8908aa5606a2365d78addfe7dfb9d5334b
gmoeser <guido.moeser@gmail.com>
2013-06-17 15:53:33 +0300

    Update 4_Sentiment Word Lists.R
    Added some comments

--- 4_Sentiment Word Lists.R
+++ 4_Sentiment Word Lists.R
@@ -1,20 +1,17 @@
-<<<<<<< HEAD
-## Load Hu & Liu's opinition lexicon
-=======
 ## Load Hu & Liu's opinion lexicon
->>>>>>> 5cdc892797eeb6d848d58c730856532f147268b0
 
 # Download Hu & Liu's opinion lexicon:
 # http://www.cs.uic.edu/~liub/FBS/opinion-lexicon-English.rar
 
 # Loading data
-
+## You should change directories!! 
 hu.liu.pos <- scan('D:/Google Drive/02_Science/Publikationen/Moeser & Moryson 2013/Opinion Lexicon Hu & Liu/positive-words.txt',
                    what = 'character', comment.char=';')
 hu.liu.neg <- scan('D:/Google Drive/02_Science/Publikationen/Moeser & Moryson 2013/Opinion Lexicon Hu & Liu/negative-words.txt',
                    what = 'character', comment.char=';')
 
 # Add a few specific and/or especially emphatic terms:
+# If you want to add some specific word expressing sentiments you can add them here!
 #pos.words <- c(hu.liu.pos, '')
 #neg.words <- c(hu-liu.neg, '')
 
