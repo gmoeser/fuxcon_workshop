68ab5dcdc792fed5bfa48d714c5c3c22cc4d2886
gmoeser <guido.moeser@gmail.com>
2013-06-17 15:54:48 +0300

    Update 6_Algorithm sanity check.R
    Changed $test into $text

--- 6_Algorithm sanity check.R
+++ 6_Algorithm sanity check.R
@@ -3,8 +3,7 @@
 # plyr and stringr must be installed!
 
 # Build small test-dataset
-test_corpus <- c(tweet_list_complete$test[3],
-tweet_list_complete$test[4])
+test_corpus <- c(tweet_list_complete$text[3], tweet_list_complete$text[4])
 
 # Function score.sentiment must be loaded!
 result <- score.sentiment(test_corpus, hu.liu.pos, hu.liu.neg)
